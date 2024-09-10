6aee47b1369dd671c694ee8b35da8f2f3cf6283b
amaan3091 <amaan.work96@gmail.com>
2024-09-10 16:43:30 +0530

    Refined Sucess message

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -20,7 +20,7 @@ router.post("/signup", async (req, res) => {
     const newUser = new User({ username, password: hashedPassword });
     await newUser.save();
 
-    res.status(201).json({ message: "User registered successfully" });
+    res.status(201).json({ message: "Wohoo!! 🎊 User registered successfully" });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
